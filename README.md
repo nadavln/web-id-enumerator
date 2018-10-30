10cf2ba56e9bd91e6ea7046eb01f6775745f2c5b
Nadav <nadav.lauden@gmail.com>
2018-10-30 13:12:24 +0200

    Made the code more readable, slightly changed README file

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # web-id-check
 A simple Python script that brute-checks if a page ID exists. (For example www.website.com/?id=345)
+
+## Instructions
+Please note that for now you need to manually enter the parameters in the code.
+
--- webidchecker.py
+++ webidchecker.py
@@ -5,6 +5,9 @@ import subprocess
 
 subprocess.call(["clear"])
 
+# Rename id_range to the ID range you want to test
+id_range = 2000
+
 # Returns HTTP status code
 def get_http_status_code(url):
 	try:
@@ -12,8 +15,8 @@ def get_http_status_code(url):
 		return r.status_code
 	except requests.ConnectionError:
 		return "Failed to connect"
-		
-for i in range(200):
+
+for i in range(id_range):
 	try:
 		# Rename the "current_check" variable to the website you want, and leave the ID field empty
 		# (like the example below)
