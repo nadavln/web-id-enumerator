c7db95a30f9e41247c7bfec6f6cadb0ae8a68285
Nadav <nadav.lauden@gmail.com>
2018-10-30 17:47:51 +0200

    Added command-line input, no code changing needed :)

--- webidchecker.py
+++ webidchecker.py
@@ -2,28 +2,33 @@
 
 import requests
 import subprocess
+import optparse
+
+parser = optparse.OptionParser()
+
+parser.add_option("-u", "--url", help="URL to test ID (do NOT include with the actual ID)", action="store",
+                  type="string", dest="url")
+parser.add_option("-r", "--range", help="ID range to test", action="store", type="int", dest="id_range")
+(options, args) = parser.parse_args()
+
 
 subprocess.call(["clear"])
 
-# Rename id_range to the ID range you want to test
-id_range = 2000
-
 # Returns HTTP status code
 def get_http_status_code(url):
-	try:
-		r = requests.head(url)
-		return r.status_code
-	except requests.ConnectionError:
-		return "Failed to connect"
+    try:
+        r = requests.head(url)
+        return r.status_code
+    except requests.ConnectionError:
+        return "Failed to connect"
 
-for i in range(id_range):
-	try:
-		# Rename the "current_check" variable to the website you want, and leave the ID field empty
-		# (like the example below)
-		current_check = "http://www.website.com/?id=" + str(i)
-		check_result = get_http_status_code(current_check)
-		if check_result != 404 and check_result != 500:
-			print(current_check + " ==> " + str(check_result))
-	except KeyboardInterrupt:
-		print("\nQuitting...")
-		break
+
+for i in range(options.id_range):
+    try:
+        current_check = options.url + str(i)
+        check_result = get_http_status_code(current_check)
+        if check_result != 404 and check_result != 500:
+            print(current_check + " ==> " + str(check_result))
+    except KeyboardInterrupt:
+        print("\nQuitting...")
+        break
